1543dd98bfbe12dc9152fc5548cd5d485fef4d83
sonyhonto <157215626+sonyhonto@users.noreply.github.com>
2024-03-05 12:21:52 +0100

    signin menu
    signin, signup

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,22 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { AuthRoutes } from './auth.routes';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+// import { AuthRoutingModule } from './auth-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { VetService } from 'app/vets/vet.service';
+import { VetResolver } from 'app/vets/vet-resolver';
 
 
 
 @NgModule({
+  imports: [
+    CommonModule,
+    // AuthRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule
+  ],
   declarations: [
     SigninComponent,
     SignupComponent,
     ForgotPasswordComponent
   ],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(AuthRoutes)
-  ]
+  exports: [
+    SigninComponent,
+    SignupComponent,
+    ForgotPasswordComponent
+  ],
+  providers: [VetService, VetResolver]
 })
 export class AuthModule { }
--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
-export class SigninComponent {
+export class SigninComponent implements OnInit {
+
+  errors: [];
+  errorsMockTrue = [{
+    "errorEffect": 'SIGN_IN',
+    "error400": {"status": 400},
+    "error401": {"status": 401},
+    "error500": {"status": 500},
+    "error0": {"status": 0},
+    "error": {"status": -1}
+  }]; 
+
+  ngOnInit() {
+
+  }
+
+  printConsole() {
+    console.log("errors mock : ", this.errorsMockTrue);
+  }
 
 }
