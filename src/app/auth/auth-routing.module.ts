4365fba6ae0afbd002ce3ee690c9b67f6f8d553c
sonyhonto <157215626+sonyhonto@users.noreply.github.com>
2024-03-27 14:16:37 +0100

    remove forgot password

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,12 +2,10 @@ import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import { SignupComponent } from 'app/auth/signup/signup.component';
 import { SigninComponent } from 'app/auth/signin/signin.component';
-import { ForgotPasswordComponent } from 'app/auth/forgot-password/forgot-password.component';
 
 const authRoutes: Routes = [
     {path: 'auth/signin', component: SigninComponent},
     {path: 'auth/signup', component: SignupComponent},
-    {path: 'auth/forgotpass', component: ForgotPasswordComponent}
 ];
 
 @NgModule({
